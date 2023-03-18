690baee3b0e0d23c468faaf53ed23c45a25f4684
Muhammad AKbarul Ihsan <barullisan@gmail.com>
2023-03-18 10:17:09 +0700

    feat: add ref to dict

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("entries/<str:title>", views.entries, name="entries"),
+    path("search", views.search, name="search"),
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,29 @@
-from django.shortcuts import render
+from django import forms
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from . import util
 
 
-def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
 
+def index(request):
+    if request.method == 'POST':
+        search = request.POST
+        return render(request, 'encyclopedia/search')
+
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
+
+def entries(request, title):
+        return render(request, "encyclopedia/entries.html", {
+        "content": util.get_entry(title)
+    }) 
+
+def search(request):
+    q = request.GET.get('q').strip()
+    return render(request, "encyclopedia/entries/q") 
+    
+      
